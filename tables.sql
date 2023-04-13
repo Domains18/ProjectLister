a6e9b5640dc9f7da14c52ad40be086d5a639f670
Domains18 <gibsonsgibson88@gmail.com>
2023-04-13 16:47:37 +0300

    sql syntaxes
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- tables.sql
+++ tables.sql
@@ -9,8 +9,17 @@ CREATE TABLE IF NOT EXISTS address(
 
 
 CREATE TABLE if NOT EXISTS voter_table (
+    VoterId INTEGER NOT NULL,
     FirstName varchar(255) NOT NULL,
     MiddleName varchar(255) NOT NULL,
     LastName VARCHAR(255) NOT NULL,
     sex char(7) NOT NULL,
-)
+    Birthday DATE NOT NULL,
+    Age integer NOT NULL,
+    Phone NUMERIC(10) NOT NULL,
+    Email varchar(255) NOT NULL,
+    DistrictId INTEGER NOT NULL,
+    CONSTRAINT PK_Voter PRIMARY KEY (VoterId),
+    CONSTRAINT FK_Voter FOREIGN KEY (DistrictId) REFERENCES address(DistrictId));
+
+
