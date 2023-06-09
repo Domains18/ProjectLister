660184ed23d6080b241ab1142075e914f13ab51c
Domains18 <gibsonsgibson88@gmail.com>
2023-06-10 02:00:35 +0300

    authentication server
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- authServer.js
+++ authServer.js
@@ -7,26 +7,14 @@ const jwt = require('jsonwebtoken')
 
 api.use(express.json());
 
-const posts = [
-    {
-        userName: 'John',
-        title: 'Post 2'
-    },
-    {
-        userName: 'Kyle',
-        title: 'Post 3'
-    },
-    {
-        userName: 'Bashir',
-        title: 'Post 4'
-    },
-]
 
-
-api.get('/posts', authenticateToken, (req, res) => {
-    // console.log(req.user.name)
-    res.json(posts.filter(post => post.userName === req.user.name))
-});
+api.post('/login', (req, res) => {
+    //authenticate user
+    const userName = req.body.userName
+    const user = { name: userName };
+    const accessToken = jwt.sign(user, process.env.JWT_SECRET);
+    res.json({ accessToken: accessToken })
+})
 
 function authenticateToken(req, res, next) {
     const authHeaders = req.headers['authorization']
@@ -43,4 +31,4 @@ function authenticateToken(req, res, next) {
 
 
 
-api.listen(3000, () => console.log('API running on port 3000'));
+api.listen(3001, () => console.log('API running on port 3001'));
