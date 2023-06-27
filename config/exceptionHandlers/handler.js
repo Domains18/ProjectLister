f9e2200f51f5435bbe56eeb8c8446a54c41a9228
Domains18 <gibsonsgibson88@gmail.com>
2023-06-27 13:50:52 +0300

    added handler js library function
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- config/exceptionHandlers/handler.js
+++ config/exceptionHandlers/handler.js
@@ -3,9 +3,18 @@ function lostErrorHandler(req, res, next) { //error 404
     res.json({
         error: "resource not found"
     });
-
-    //exception handler
-    function AppErrorHandler(req, res, next) {
-        res.status()
-    }
 }
+//exception handler
+function AppErrorHandler(req, res, next) {
+    res.status(err.status || 500);
+    if (err.authorizationError == true) {
+        //setting available headers
+        res.set(err.authHeaders)
+    }
+    const error = err?.cause || err?.message;
+    const provideFeedback = err?.feedback;
+
+    res.json({ error, ...AppErrorHandler(provideFeedback && { feedback: provideFeedback }) });
+}
+
+module.exports = { lostErrorHandler, AppErrorHandler }
