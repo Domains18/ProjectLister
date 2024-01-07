3f5b99e53b8528c0a7987538dc2b322e84c70d97
Domains18 <gibsonsgibson88@gmail.com>
2024-01-08 00:57:03 +0300

    structure setup

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const http = require('http');
+require('dotenv').config();
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+app.use('/', function (req, res) {
+    if (req.accepts("html")) {
+        res.sendFile(__dirname + "/public/index.html");
+    } else if (req.accepts("json")) {
+        res.json({message: "Backend Admin API reached, please make sure you know what you are doing"});
+    } else {
+        return res.sendStatus(404)
+    }
+});
+
+
+const server = http.createServer(app);
+Promise.resolve()
+    .then(() => {
+        server.listen(port, () => {
+            console.log(`Server is listening on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
